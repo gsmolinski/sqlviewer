3ca769e6da7c36a5c5ec52ab22f99bc5c19ade6a
gsmolinski <g.smolinski1@gmail.com>
2024-09-01 10:52:58 +0200

    update packages

--- inst/app/run_dev_app.R
+++
@@ -1,7 +0,0 @@
-# devtools::load_all()
-# future::plan("multisession")
-# open(RSQLite::SQLite(), "~/test2.db", launch_browser = .rs.invokeShinyPaneViewer)
-# #
-# # # mydb <- DBI::dbConnect(RSQLite::SQLite(), dbname = "~/test2.db")
-# # # DBI::dbWriteTable(mydb, "iris", dplyr::bind_rows(iris, iris, iris, iris, iris, iris, iris, iris, iris, iris, iris), overwrite = TRUE)
-# # # DBI::dbDisconnect(mydb)
