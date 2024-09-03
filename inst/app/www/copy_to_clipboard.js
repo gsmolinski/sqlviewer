96fddeb248e79bf0d056844ca94685c9f2f7fd42
gsmolinski <g.smolinski1@gmail.com>
2024-09-03 18:41:16 +0200

    add window.navigator instead of just navigator

--- inst/app/www/copy_to_clipboard.js
+++ inst/app/www/copy_to_clipboard.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
   Shiny.addCustomMessageHandler("copy_to_clipboard", function(sql_query) {
-    navigator.clipboard.writeText(sql_query);
+    window.navigator.clipboard.writeText(sql_query);
   });
 });
--- inst/app/www/read_from_clipboard.js
+++ inst/app/www/read_from_clipboard.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
   Shiny.addCustomMessageHandler("read_from_clipboard", function(placeholder) {
     if (document.hasFocus()) {
-      navigator.clipboard
+      window.navigator.clipboard
         .readText()
         .then((clipText) => (Shiny.setInputValue("js_clipboard", clipText)));
     }
